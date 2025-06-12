5c5b1daa1edbeccb8896457155fb5c32ccc3037b
Marianie Vásquez <Vasquezmarianie@gmail.com>
2025-06-11 20:00:33 -0500

    Modificacion de carpetas

---
+++ Clase 3 /vite-project/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+       <main className="h-screen w-full bg-gray-950 flex flex-col justify-center items-center gap-4">
+      <h1 className="text-5xl font-bold text-white">
+        React + Vite + Tailwind CSS
+      </h1>
+      <p className="text-xl text-emerald-400">
+        Instalación con @tailwindcss/vite ¡funcionando!
+      </p>
+    </main>
+  )
+}
+
+export default App
